eaa114f72ae7f1a98b901a82358159bf81330576
DfE-Hoyage <andrew.hoy@education.gov.uk>
2025-01-17 17:04:04 +0000

    New Version

--- game.py
+++ game.py
@@ -5,35 +5,39 @@ def input_users_name():
     return player_name
 
 def user_answer(player_name,symbol,num1,num2):
-    repeat = True
-    while repeat:
+    repeat = True 
+    while repeat: #Repeats the below until the user inputs a whole number
         print("Please answer the question", num1, symbol, num2, "=")
-        try:
+        try: #User validation check for whole number
             useranswer = int(input("Hello " + player_name + " What is your answer?"))
             return useranswer 
         except ValueError:
             print("Please enter a whole number!\n")
             repeat=True
 
+#Function for incrementing the score and printing it out when the user gets a question correct
 def correct(score):
         score += 1
         print("You got the answer right!\nYour score is now", score)
         return score
 
+#Function for printing out the score when the user gets a question wrong
 def incorrect(score):
         print("You got the answer wrong!\nYour score is still", score)    
         return score
 
+# Add question function
 def add(score,player_name,num1,num2):
     symbol="+"
-    useranswer=user_answer(player_name,symbol,num1,num2)
-    if num1+num2==useranswer:
+    useranswer=user_answer(player_name,symbol,num1,num2) #Calls the function to request user answer to the question.
+    if num1+num2==useranswer: #Checks users answer is correct and calls the correct or incorrect answer function.
         score=correct(score)
         return score
     else:
         score=incorrect(score)
         return score
-
+    
+#Subtraction question function
 def sub(score,player_name,num1,num2):
     symbol="-"
     useranswer=user_answer(player_name,symbol,num1,num2)
@@ -43,7 +47,8 @@ def sub(score,player_name,num1,num2):
     else:
         score=incorrect(score)
         return score
-       
+    
+#Multiplication question function       
 def times(score,player_name,num1,num2):
     symbol="*"
     useranswer=user_answer(player_name,symbol,num1,num2)
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import random
+#Imports all the required functions form the game.py file
 from game import add, sub, times, input_users_name
 
+#Set the required varibles to the start numbers.
 round = 1
 score = 0
-
+#Calls the function to request the player sets a name
 player_name = input_users_name()
 
+#While loop runs well round doesn't = 11 which results in the loop running 10 times for 10 questions
 while round != 11:
     print("Question number", round)
-    question = random.randint(1,1)
+    question = random.randint(1,3) #Randomly generates a number between 1 and 3 to pick the question type
     num1 = random.randint(0,10)
     num2 = random.randint(0,10)
-    if question == 1:
-        score=add(score,player_name,num1,num2)
+    if question == 1: #If number generated was one runs the function for the add question
+        score=add(score,player_name,num1,num2) 
     elif question == 2:
         score=sub(score,player_name,num1,num2)
     elif question == 3:
         score=times(score,player_name,num1,num2)
-    round += 1
-print("Your final score is", score)
+    round += 1 #Increments the round number by 1
+print("Your final score is", score) #Prints final score after the 10th round
 
