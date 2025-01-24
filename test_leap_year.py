97b7131a773a037aab3419cd4c939d76769312e6
DfE-Hoyage <andrew.hoy@education.gov.uk>
2025-01-24 15:18:07 +0000

    TDD

--- test_leap_year.py
+++ test_leap_year.py
@@ -1,7 +1,7 @@
 from leap_year import leap_year_check
 
 def test_leap_year_ok():
-    assert 2024 ==True
+    assert leap_year_check(2024) ==True
 
 def test_leap_year_wrong():
-    assert 2025 == False
+    assert leap_year_check(2025) == False
